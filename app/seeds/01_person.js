3f7acc325fafaf5d5b5759161a76fac4b36eb384
Danielle Maier <danielle.e.maier@gmail.com>
2017-09-20 13:31:09 -0600

    added password excryption

--- app/seeds/01_person.js
+++ app/seeds/01_person.js
@@ -1,20 +1,26 @@
+require('dotenv').config();
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+
+
 exports.seed = function(knex, Promise) {
-    // Deletes ALL existing entries
-    return knex.raw('DELETE FROM "person"; ALTER SEQUENCE person_id_seq RESTART WITH 3;')
-        .then(function() {
-            var person = [{
-                    id: 1,
-                    name: "Danielle Maier",
-                    email: "danielle@example.com",
-                    password: "travels"
-                },
-                {
-                    id: 2,
-                    name: "Mikki Mesfin",
-                    email: "mikki@example.com",
-                    password: "departure"
-                }
-            ];
-            return knex('person').insert(person);
-        })
+  return knex.raw('DELETE FROM "person"; ALTER SEQUENCE person_id_seq RESTART WITH 3;')
+    .then(function() {
+      var hash = bcrypt.hashSync(process.env.SECRET_WORD, saltRounds)
+      var hash2 = bcrypt.hashSync(process.env.SECRET_WORD2, saltRounds)
+      var person = [{
+          id: 1,
+          name: "Danielle Maier",
+          email: "danielle@example.com",
+          password: hash
+        },
+        {
+          id: 2,
+          name: "Mikki Mesfin",
+          email: "mikki@example.com",
+          password: hash2
+        }
+      ];
+      return knex('person').insert(person);
+    })
 };
